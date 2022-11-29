74f7727b82ff32d84091dd019c5066face795584
Zoey Anastasiadis <zcanast@gmail.com>
2022-11-29 01:07:19 -0500

    updates

--- script.js
+++ script.js
@@ -39,24 +39,8 @@ function initChart(chart, object) {
     labels: labels,
     datasets: [{
       label: 'Crimes By Category',
-      backgroundColor: [
-      'rgba(255, 99, 132)',
-      'rgba(255, 159, 64)',
-      'rgba(255, 205, 86)',
-      'rgba(75, 192, 192)',
-      'rgba(54, 162, 235)',
-      'rgba(153, 102, 255)',
-      'rgba(201, 203, 207)'
-      ],
-      borderColor: [
-      'rgb(255, 99, 132)',
-      'rgb(255, 159, 64)',
-      'rgb(255, 205, 86)',
-      'rgb(75, 192, 192)',
-      'rgb(54, 162, 235)',
-      'rgb(153, 102, 255)',
-      'rgb(201, 203, 207)'
-      ],
+      backgroundColor: 'rgba(255, 99, 132)',
+      borderColor: 'rgb(201, 203, 207)',
       data: info
     }]
   };
